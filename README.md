089c0f41b5835e9bfe4d023c45306705c8dfd9c6
Adam Ai <neuroglyph.ai@gmail.com>
2025-01-23 13:23:30 +0700

    [add] readme and run cmd

---
+++ README.md
@@ -0,0 +1,11 @@
+# Wavvy AI SDK
+
+Run with Apple CPU
+
+```bash
+cargo run --features metal \
+-- \
+--model-path ./model/Qwen2.5-3B-Instruct/qwen2.5-3b-instruct-q4_0.gguf \
+--tokenizer-path ./model/Qwen2.5-3B-Instruct/tokenizer.json \
+--prompt "1+1"
+```
--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,95 +1,102 @@
+use clap::Parser;
+
 use candle_core::Device;
-// use futures::StreamExt;
+use futures::StreamExt;
 use wavvy_engine::{
-    llm::{model_builder::ModelBuilder, wavvy_chat::WavvyChat},
+    llm::{model_builder::ModelBuilder, wavvy_chat::WavvyChat, wavvy_chat_stream::WavvyArgs},
     prompt_template::{
         chat_template::ChatTemplate,
         message::{Message, Role},
     },
 };
 
-// #[tokio::main]
-// async fn run_stream() {
-//     let assistant = Message {
-//         role: Role::System,
-//         content: String::from("You are helpful assistant!"),
-//     };
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(long)]
+    pub model_path: Option<String>,
 
-//     let question = "What is 1+1";
+    #[arg(long)]
+    pub tokenizer_path: Option<String>,
 
-//     let messages = vec![
-//         assistant,
-//         Message {
-//             role: Role::User,
-//             content: String::from(question),
-//         },
-//     ];
+    #[arg(long)]
+    pub prompt: Option<String>,
 
-//     let message_template = ChatTemplate::new(messages);
+    #[arg(short = 'n', long, default_value_t = 1000)]
+    pub sample_len: usize,
 
-//     let device = Device::new_metal(0).unwrap();
-//     let model_builder = ModelBuilder::new(
-//         "./model/Qwen2.5-3B-Instruct/qwen2.5-3b-instruct-q4_0.gguf",
-//         "./model/Qwen2.5-3B-Instruct/tokenizer.json",
-//         &device,
-//     );
+    #[arg(long, default_value_t = 0.8)]
+    pub temperature: f64,
 
-//     let tokenizer = model_builder.load_tokenizer();
-//     let model = model_builder.load_model();
-//     println!("Model and tokenizer loaded");
+    #[arg(long)]
+    pub top_p: Option<f64>,
 
-//     println!("Question: {question}");
-//     print!("Answer: ");
-//     let wavvy = WavvyChat::new(model, tokenizer, &device, None);
-//     let mut response = wavvy.stream_invoke(message_template.format()).unwrap();
-//     while let Some(item) = response.next().await {
-//         match item {
-//             Ok(response) => {
-//                 print!("{}", response.content);
-//             }
-//             Err(e) => {
-//                 println!("Error: {}", e);
-//             }
-//         }
-//     }
-// }
+    #[arg(long)]
+    pub top_k: Option<usize>,
 
-fn main() {
-    let assistant = Message {
-        role: Role::System,
-        content: String::from("You are helpful assistant!"),
-    };
+    #[arg(long, default_value_t = 299792458)]
+    pub seed: u64,
 
-    let question = "What is 1+1";
+    #[arg(long)]
+    pub split_prompt: bool,
 
-    let messages = vec![
-        assistant,
-        Message {
-            role: Role::User,
-            content: String::from(question),
-        },
-    ];
+    #[arg(long, default_value_t = 1.1)]
+    pub repeat_penalty: f32,
+
+    #[arg(long, default_value_t = 64)]
+    pub repeat_last_n: usize,
+}
+
+#[tokio::main]
+async fn main() {
+    let args = Args::parse();
+
+    let question = args.prompt.unwrap();
+
+    let messages = vec![Message {
+        role: Role::User,
+        content: question.clone(),
+    }];
 
     let message_template = ChatTemplate::new(messages);
 
     let device = Device::new_metal(0).unwrap();
+    // Path examples:
+    // model-path: ./model/Qwen2.5-3B-Instruct/qwen2.5-3b-instruct-q4_0.gguf
+    // tokenizer-path: ./model/Qwen2.5-3B-Instruct/tokenizer.json
     let model_builder = ModelBuilder::new(
-        "./model/Qwen2.5-3B-Instruct/qwen2.5-3b-instruct-q4_0.gguf",
-        "./model/Qwen2.5-3B-Instruct/tokenizer.json",
+        args.model_path.unwrap().as_str(),
+        args.tokenizer_path.unwrap().as_str(),
         &device,
     );
-
-    println!("Wavvy initializing...");
 
     let tokenizer = model_builder.load_tokenizer();
     let model = model_builder.load_model();
     println!("Model and tokenizer loaded");
 
-    let wavvy = WavvyChat::new(model, tokenizer, &device, None);
+    let wavvy_args = Some(WavvyArgs {
+        sample_len: args.sample_len,
+        temperature: args.temperature,
+        top_p: args.top_p,
+        top_k: args.top_k,
+        seed: args.seed,
+        split_prompt: args.split_prompt,
+        repeat_penalty: args.repeat_penalty,
+        repeat_last_n: args.repeat_last_n,
+    });
+    let wavvy = WavvyChat::new(model, tokenizer, &device, wavvy_args);
+    let mut response = wavvy.stream_invoke(message_template.format()).unwrap();
+
     println!("Question: {question}");
-    let response = wavvy.invoke(message_template.format()).unwrap();
-    println!("Answer: {}", response.content);
-    println!("Prompt Tokens: {}", response.prompt_tokens);
-    println!("Completion Tokens: {}", response.completion_tokens);
-    println!("Total Tokens: {}", response.total_tokens);
+    print!("Answer: ");
+    while let Some(item) = response.next().await {
+        match item {
+            Ok(response) => {
+                print!("{}", response.content);
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+            }
+        }
+    }
 }
